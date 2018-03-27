0f4384e834b4b74adb739680c887f3c9742caf3c
Brandon LeMay <blemay360@gmail.com>
2018-03-26 20:50:32 -0400

    Notes for Mar 26

--- test.py
+++ test.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python3
-base=input("Please enter base pairs: ")
-complimentary=''
-for i in range(len(base)):
-	if i>0 and i%4==0:
-		complimentary+=' '
-	c = base[i]
-	c=c.lower()
-	if (c == 'g'):
-		complimentary+='C'
-	elif (c == 'c'):
-		complimentary+='G'
-	elif (c == 'a'):
-		complimentary+='T'
-	elif (c == 't'):
-		complimentary+='A'
+def main(val=0):
+	if val==0: 
+		return("Hello world");
 	else:
-		print("Break out the Head & Shoulders")
-		break
-print(complimentary)
+		return("Hi universe");
+
+if __name__=="__main__":
+	print(main(int(input())));
