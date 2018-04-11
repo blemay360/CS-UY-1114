1034212f6778c7ea55afe32580a1172b885cfb06
Brandon LeMay <blemay360@gmail.com>
2018-04-11 16:30:25 -0400

    Notes for Apr 11

--- Notes/view.sh
+++ Notes/view.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+counter=0
 for file in ~/CS-UY1114/Notes/*.txt; do
+#	days[$counter]=$file
+#	let counter=$counter+1
 	cat $file
 done
+#for i in ${#days[@]}; do
+#	echo ${days[i]}
+#done
